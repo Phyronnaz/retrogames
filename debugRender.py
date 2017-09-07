f7ab66181f2cb60e2b11b7116e33663f2206f0fe
quentinLeDilavrec <ledilavrecquentin@gmail.com>
2017-09-07 16:29:30 +0200

    quadTreeRender

--- debugRender.py
+++ debugRender.py
@@ -1,6 +1,20 @@
 from Quadtree import *
-import turtle
-def squareRender(q:QuadTree):
-    turtle.goto(q.x, q.y)
-    turtle.()
-    turtle.color(self.color)
+import pygame
+
+def quadTreeRender(screen:pygame.Surface,q:QuadTree):
+    print(self.position)
+    w=q.width()/2
+    x1=self.position[0]-w
+    x2=self.position[0]+w
+    y1=self.position[1]-w
+    y2=self.position[1]+w
+
+    pygame.draw.line(screen, (0,0,0), (x1,y1), (x1,y2), width=1)
+    pygame.draw.line(screen, (0,0,0), (x1,y2), (x2,y2), width=1)
+    pygame.draw.line(screen, (0,0,0), (x2,y2), (x2,y1), width=1)
+    pygame.draw.line(screen, (0,0,0), (x2,y1), (x2,y1), width=1)
+
+
+    if not q.is_leaf:
+        for child in q.childs:
+            quadTreeRender(screen, child)
