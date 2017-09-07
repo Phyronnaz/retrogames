ac0da80e00f32f2da8992a028ef83dd7e7509ace
Phyronnaz <phyronnaz@gmail.com>
2017-09-07 17:13:29 +0200

    Things

--- debug_render.py
+++ debug_render.py
@@ -4,12 +4,12 @@ from Quadtree import *
 import pygame
 
 
-def quadtree_render(screen: pygame.Surface, q: Quadtree):
-    print(q.position)
-    w = q.width()
+def quadtree_render(screen: pygame.Surface, quadtree: Quadtree):
+    # print(q.position)
+    w = quadtree.width()
 
-    pygame.draw.rect(screen, (0, 0, 0), Rect(q.position, (w, w)), 0)
+    pygame.draw.rect(screen, (0, 255, 0), Rect(quadtree.position, (w, w)), 10)
 
-    if not q.is_leaf:
-        for child in q.childs:
+    if not quadtree.is_leaf:
+        for child in quadtree.childs:
             quadtree_render(screen, child)
